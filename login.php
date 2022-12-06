a90c18de499d1e3b6095b8c7dec6795554ec3430
gaetanvan <61145812+gaetanvan@users.noreply.github.com>
2022-12-06 16:59:31 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,32 +1,31 @@
-        <?php
-        session_start();
-
-            if (isset($_POST['submit']))
-            {
-                $mail = $_POST['mail'];
-                $pass = $_POST['pass'];
-
-                $db = new PDO('mysql:host=localhost;dbname=animodo', 'root', '');
-
-                $sql = "SELECT * FROM user where mail = '$mail'";
-                $result = $db->prepare($sql);
-                $result->execute();
-
-                if ($result->rowCount() > 0)
-                {
-                    $data = $result->fetchAll();
-                    if (password_verify($pass, $data[0]['password']))
-                    {
-                        echo "Connexion efféctué";
-                        $_SESSION['mail'] = $mail;
-                    }
-                }
-                else
-                {
-                    $pass = password_hash($pass, PASSWORD_DEFAULT);
-                    $sql = "INSERT INTO user (mail, password) VALUES ('$mail','$pass')";
-                    $req = $db->prepare($sql);
-                    $req->execute();
-                    echo "Vous êtes inscrit";
-                }
-            }
+<?php
+    session_start();
+    if (!$_SESSION['password']){
+        header('Location:index.php');
+    }
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Animodo</title>
+        <link rel="stylesheet" type="text/css" href="css/acceuil.css">
+        <link rel="stylesheet" type="text/css" href="css/login.css">
+    </head>
+    <body>
+        <div id="rectangle-color" class="rectangle1">
+            <div class="pet1">
+                <img class="petimg" src="img/chat.jpg">
+                <p class="name">Nom : chat1</p>
+                <p class="age">Age : 3 ans<br>Poids : 15kg</p>
+                <p class="meetings">Prochain vaccin : 20/07/1999</p>
+            </div>
+        </div>
+        <div id="rectangle-color" class="rectangle2">
+            <h2>Rendez-Vous :</h2>
+            <p class="name">Nom : chat1</p>
+        </div>
+    </body>
+    <footer>
+            <a href="logout.php" ><button name="logout">Logout</button></a>
+    </footer>
