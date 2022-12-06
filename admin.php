bfed16bd155a7d2fcd357968ab887e2d4b9df199
gaetanvan <61145812+gaetanvan@users.noreply.github.com>
2022-12-06 16:59:46 +0100

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if (!$_SESSION['password']){
+    header('Location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Animodo</title>
+    <link rel="stylesheet" type="text/css" href="css/acceuil.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/admin.css">
+</head>
+<body>
+<div id="rectangle-color" class="adminRectangle">
+    <div class="userAdd">
+        <a href="userCreate.php">
+            <button>Nouvelle utilisateur</button>
+        </a>
+    </div>
+    <div class="pet1">
+        <img class="petimg" src="img/chat.jpg">
+        <p class="name">Nom : chat1</p>
+        <p class="age">Age : 3 ans<br>Poids : 15kg</p>
+        <p class="meetings">Prochain vaccin : 20/07/1999</p>
+    </div>
+</div>
+<div id="rectangle-color" class="rectangle2">
+    <h2>Rendez-Vous :</h2>
+    <p class="name">Nom : chat1</p>
+</div>
+</body>
+<footer>
+    <a href="logout.php" ><button name="logout">Logout</button></a>
+</footer>
